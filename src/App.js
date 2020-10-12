689b879c410c6db679300b687a0fb3c71701bc78
kureharyosuke <ohhayo.com@gmail.com>
2020-10-13 01:31:07 +0900

    #3. Navbar Set Up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 
 import "./App.css";
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,64 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom ";
-import { Button } from "./Button";
-import { Dropdown } from "./Dropdown";
+import { Link } from "react-router-dom";
+import Button from "./Button";
+import Dropdown from "./Dropdown";
+import { FaPlug, FaTimes, FaStream, FaCaretDown } from "react-icons/fa";
 import "./Navbar.css";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
   return (
     <>
       <nav className="navbar">
         <Link to="/" className="navbar-logo">
           Dropdown
+          <FaPlug />
         </Link>
         <div className="menu-icon" onClick={handleClick}>
-          <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          {click ? <FaTimes /> : <FaStream />}
         </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/services"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Services
+              <FaCaretDown />
+            </Link>
+            {dropdown && <Dropdown />}
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/contact-us"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Contact us
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/sign-up"
+              className="nav-links-modbile"
+              onClick={closeMobileMenu}
+            >
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+        <Button />
       </nav>
     </>
   );
